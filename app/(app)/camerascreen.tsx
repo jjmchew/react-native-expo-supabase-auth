7827df70736abdf9d1385ec7c74c01609a52bbcd
James Chew <jjmchew@gmail.com>
2025-11-25 12:29:55 -0700

    fix: update profile pic from camera

--- app/(app)/camerascreen.tsx
+++ app/(app)/camerascreen.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Camera from "@/components/Camera";
 import { View, Text } from "react-native";
 import { Stack, router } from "expo-router";
@@ -5,23 +6,25 @@ import { useFetchImage } from "@/hooks/useFetchImage";
 import { useSupabase } from "@/hooks/useSupabase";
 
 export default function CameraScreen() {
+  const [uri, setUri] = useState<string | undefined>(undefined);
   const { uploadAvatar: supabaseUpload, updateProfile } = useSupabase();
   const { loading, fetchImageForUpload } = useFetchImage();
 
   const handleUpload = async (filePath: string) => {
-    router.push("/home");
     try {
+      setUri(filePath);
       const rawUploadData = await fetchImageForUpload({
         uri: filePath,
       });
       const data = await supabaseUpload(rawUploadData);
       await updateProfile({ avatar_url: data.path });
+      router.push("/home");
     } catch (err) {
       console.error("CameraScreen err", err);
     }
   };
 
-  if (loading)
+  if (loading || uri)
     return (
       <View>
         <Text>loading...</Text>
